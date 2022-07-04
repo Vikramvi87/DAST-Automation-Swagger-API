0671e14e7a4d60b639a2096cab6dcf148ea20538
jrocia <69405400+jrocia@users.noreply.github.com>
2022-07-04 12:01:17 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@ apiKeyId='xxxxxxxxxxxxxxxxx'
 apiKeySecret='xxxxxxxxxxxxxxxxx'
 appId='xxxxxxxxxxxxxxxxx'
 appscanPresenceId='xxxxxxxxxxxxxxxxx'
-urlTarget='https://abcd.com'
+urlTarget='https://www.abcd.com'
+baseUrl='https://www.abcd.com/api'
 jsonPath='/swagger/v3/swagger.json'
 
 cd TrafficRecorder/
@@ -20,7 +21,7 @@ openapi2postmanv2 -s targetswagger.json -o output.json
 # Set proxy to 55555 port
 export http_proxy=http://localhost:55555/ && export https_proxy=http://localhost:55555/
 # Execute the collection
-newman run output.json --env-var baseUrl="$urlTarget" --insecure
+newman run output.json --env-var baseUrl="$baseUrl" --insecure
 # Stop proxy
 curl -H 'Accept: application/json' 'http://localhost:8383/automation/StopProxy/55555'
 sleep 10
@@ -29,15 +30,21 @@ export http_proxy= && export https_proxy=
 # Get the manual explorer file from Server Proxy
 curl -X GET "http://localhost:8383/automation/Traffic/55555" -H  "accept: application/json" --output dast.config
 # Kill Server Proxy
+sleep 10
 pkill -9 node
+
+# Update urltarget in scan template
+sed -i "s#http://www.abcd.net#$urlTarget#" scantdomfilteringfalse.scant
 
 # Authenticate in ASoC
 asocToken=$(curl -s -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d '{"KeyId":"'"${apiKeyId}"'","KeySecret":"'"${apiKeySecret}"'"}' 'https://cloud.appscan.com/api/V2/Account/ApiKeyLogin' | grep -oP '(?<="Token":")[^"]*')
+# Upload the scan template file
+scantFileId=$(curl -X 'POST' 'https://cloud.appscan.com/api/v2/FileUpload' -H 'accept: application/json' -H "Authorization: Bearer $asocToken" -H 'Content-Type: multipart/form-data' -F 'fileToUpload=@scantdomfilteringfalse.scant;type=application/xml' | grep -oP '(?<="FileId":")[^"]*')
 # Upload the manual explorer file
 dastFileId=$(curl -X 'POST' 'https://cloud.appscan.com/api/v2/FileUpload' -H 'accept: application/json' -H "Authorization: Bearer $asocToken" -H 'Content-Type: multipart/form-data' -F 'fileToUpload=@dast.config;type=application/xml' | grep -oP '(?<="FileId":")[^"]*')
 date=$(date '+%m-%d-%Y')
 # Start the scan
-scanId=$(curl -s -X 'POST' 'https://cloud.appscan.com/api/v2/Scans/DynamicAnalyzerWithFiles' -H 'accept: application/json' -H "Authorization: Bearer $asocToken" -H 'Content-Type: application/json' -d  '{"StartingUrl":"'"$urlTarget"'","TestOnly":true,"ExploreItems":[{"FileId":"'"$dastFileId"'","MultiStep": false}],"LoginUser":"","LoginPassword":"","TestPolicy":"Default.policy","ExtraField":"","ScanType":"Staging","PresenceId":"'"$appscanPresenceId"'","IncludeVerifiedDomains":false,"HttpAuthUserName":"","HttpAuthPassword":"","HttpAuthDomain":"","TestOptimizationLevel":"Fastest","LoginSequenceFileId":"","ThreadNum":10,"ConnectionTimeout":null,"UseAutomaticTimeout":true,"MaxRequestsIn":null,"MaxRequestsTimeFrame":null,"ScanName":"'"DAST $date $urlTarget"'","EnableMailNotification":false,"Locale":"en","AppId":"'"$appId"'","Execute":true,"Personal":false,"ClientType":"user-site","Comment":null,"FullyAutomatic":false,"RecurrenceRule":null,"RecurrenceStartDate":null}' | jq -r '. | {Id} | join(" ")')
+scanId=$(curl -s -X 'POST' 'https://cloud.appscan.com/api/v2/Scans/DynamicAnalyzerWithFiles' -H 'accept: application/json' -H "Authorization: Bearer $asocToken" -H 'Content-Type: application/json' -d '{"ScanTemplateFileId":"'"$scantFileId"'","ExploreItems":[{"FileId":"'"$dastFileId"'"}],"AppId":"'"$appId"'","TestOnly":true,"StartingUrl":"'"$urlTarget"'","PresenceId":"'"$appscanPresenceId"'","TestOptimizationLevel":"NoOptimization","ScanName":"'"DAST $date $urlTarget"'","EnableMailNotification":false,"Execute":true,"Personal":false,"ClientType":"user-site","Locale":"en","Comment":null,"FullyAutomatic":true,"RecurrenceRule":null,"RecurrenceStartDate":null,"RecurrenceEndDate":null}' | jq -r '. | {Id} | join(" ")')
 
 # Loop waiting the scan finish
 for x in $(seq 1 1000)
